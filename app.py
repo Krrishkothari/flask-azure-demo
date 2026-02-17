500f741bbcc184956579ee8443f9985a1a8d72b4
Krrish Kothari <krrishbkothari@gmail.com>
2026-02-17 13:37:08 +0530

    3rd commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask app running on Azure 🚀"
+    return "krrish ko full marks doo sir pls"
 
 if __name__ == "__main__":
     app.run()
